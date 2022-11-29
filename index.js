75d03ea53f8ce5763e188102a824d5eb51d078bd
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2022-11-30 01:45:07 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 
 // Write your code here
+const chai = 'chai';
+const expect = chai.expect;
+
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+let random = 1;
+function random(){
+   return (Math.random()+1);
+} 
+
+let num3 = 20;
+let num4 = 5;
+let mod = num3 / num4;
+
+let max=20;
+function max(){
+    console.log(Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
+}
+
+
+
+
+
+
+/*
+Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+Create a variable called random that will generate a random integer greater than 0.
+Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+Create a variable called max that finds the highest number in a set; the value returned should be 20.
+*/
